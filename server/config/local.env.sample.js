7f12e0ebeff1f995d58362dbda37651364d8275d
math29 <maaath29@gmail.com>
2015-10-05 23:08:13 +0200

    Fixe merge problem

--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 // Use local.env.js for environment variables that grunt will set when the server starts locally.
@@ -22,28 +21,3 @@ module.exports = {
   // Control debug level for modules using visionmedia/debug
   DEBUG: ''
 };
-=======
-'use strict';
-
-// Use local.env.js for environment variables that grunt will set when the server starts locally.
-// Use for your api keys, secrets, etc. This file should not be tracked by git.
-//
-// You will need to set these on the server you deploy to.
-
-module.exports = {
-  DOMAIN:           'http://localhost:9000',
-  SESSION_SECRET:   'wtc-secret',
-
-  FACEBOOK_ID:      'app-id',
-  FACEBOOK_SECRET:  'secret',
-
-  TWITTER_ID:       'app-id',
-  TWITTER_SECRET:   'secret',
-
-  GOOGLE_ID:        'app-id',
-  GOOGLE_SECRET:    'secret',
-
-  // Control debug level for modules using visionmedia/debug
-  DEBUG: ''
-};
->>>>>>> ff4b3a61dda553f042e6dcba4dd87f7fe7df06c0
