58ea8af0eb8c78b9967e2ab1c3245980a08b960a
math29 <maaath29@gmail.com>
2015-12-12 21:02:02 +0100

    Integrate index page new design

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,27 +1,20 @@
 'use strict';
 
 angular.module('wtcApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    .controller('MainCtrl', function ($scope, $http, socket) {
+        $scope.myInterval = 5000;
+        $scope.noWrapSlides = false;
+        var slides = $scope.slides = [];
+        $scope.addSlide = function() {
+            var newWidth = 600 + slides.length + 1;
+            slides.push({
+                image: '//placekitten.com/' + newWidth + '/300',
+                text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+                ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+            });
+        };
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+        for (var i=0; i<4; i++) {
+            $scope.addSlide();
+        }
     });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
-  });
