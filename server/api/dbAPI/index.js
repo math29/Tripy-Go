7306732c1256fbea686fbd0b5e1549e95c9cbb58
breizhers <yoann.diquelou@gmail.com>
2015-12-17 01:21:17 +0100

    Set mongo API to adminOnly and improve documentation

--- server/api/dbAPI/index.js
+++ server/api/dbAPI/index.js
@@ -18,127 +18,135 @@ var router = express.Router();
 
 
 /**
- *  @apiDefine ApiParams
- *
- *
- * @apiParam {String} country_code   Country code abreviation
- * @apiParam {String} country_name   Name of the country
- * @apiParam {String} capital        Capital of the country
- * @apiParam {String} continent      Continent where the country is
- * @apiParam {String} languages      List of language of the continents
- * @apiParam {Number} population     Number of inhabitants
- * @apiParam {Number} area           Country's area in sqare meters
- */
-
-/**
- * @api {get} /api/countries Request list of all countries
+ * @api {get} /api/back/db Request stats of mongo database
  * @apiVersion 1.0.0
- * @apiName GetCountries
- * @apiGroup Countries
+ * @apiName GetStats
+ * @apiGroup MongoDB
  *
- * @apiSuccess {Array} array of countries
+ * @apiSuccess {Object} mongoDB stats
  *
  * @apiSuccessExample Success-Response:
  *   HTTP/1.1 200 OK
- *  [
+ *
  *    {
- *      "country_code":"CO",
- *      "country_name":"Country",
- *      "currency_code":"EUR",
- *      "population":654321,
- *      "capital":"Kingstown",
- *      "continent":"North America",
- *      "area":389,
- *      "languages":"en-VC,fr"
- *    }, ...
+ *      "collections":8,
+ *      "avgObjectSize":268.5678,
+ *        ...
+ *    }
  *  ]
  *
  * @apiUse UserNotAuthorized
  */
-router.get('/',  controller.stats);
+router.get('/',  auth.hasRole('admin'),controller.stats);
 
 /**
-  * @api {get} /api/countries/:cat/:name Request list of country where category match name
-  * @apiName GetCountriesMatch
-  * @apiGroup Countries
-  *
-  * @apiParam {String} cat  category select which column you want as criteria
-  * @apiParam {String} name   name you want for column matches
-  *
-  * @apiSuccess {Array} array of countries
-  *
-  * @apiSuccessExample Success-Response:
-  *   HTTP/1.1 Success-Response:
-  *   [
-  *      {
-  *        "_id": "56311da5daca79981b662afd",
-  *        "country_code": "VA",
-  *        "country_name": "Vatican City",
-  *        "currency_code": "EUR",
-  *        "population": 921,
-  *        "capital": "Vatican",
-  *        "continent": "Europe",
-  *        "area": 0.44,
-  *        "languages": "la,it,fr",
-  *        "__v": 0
-  *    },
-  *    ...
-  *    ]
-  *
-  *
-  */
-router.get('/names', controller.getCollectionNames);
+ * @api {get} /api/back/db/names Request list of mongo collections names
+ * @apiVersion 1.0.0
+ * @apiName GetCollectionNames
+ * @apiGroup MongoDB
+ *
+ * @apiSuccess {Array} mongoDB collection names
+ *
+ * @apiSuccessExample Success-Response:
+ *   HTTP/1.1 200 OK
+ *
+ *    ['logs','users',...
+ *  ]
+ *
+ * @apiUse UserNotAuthorized
+ */
+router.get('/names', auth.hasRole('admin'),controller.getCollectionNames);
 
 /**
-  * @api {post} /api/countries Insert a country in database
-  * @apiName InsertCountry
-  * @apiGroup Countries
-  *
-  * @apiUse ApiParams
-  *
-  * @apiSuccess {Object} Object response
-  *
-  * @apiSuccessExample Success-Response:
-  *   HTTP/1.1 201 Created:
-  *      {
-  *         "ok":1,
-  *         "nModified":0,
-  *         "upserted":[
-  *           {
-  *             "index":0,
-  *             "_id":"34347838758deb5"
-  *           }
-  *         ]
-  *     }
-  *
-  */
-router.get('/stats',  controller.allStats);
+ * @api {get} /api/back/db/stats Request stats of all mongo collections
+ * @apiVersion 1.0.0
+ * @apiName GetAllStats
+ * @apiGroup MongoDB
+ *
+ * @apiSuccess {Array} mongoDB stats
+ *
+ * @apiSuccessExample Success-Response:
+ *   HTTP/1.1 200 OK
+ *    [
+ *    { ns: 'locations',
+ *         count: 0,
+ *         size: 0,
+ *         numExtents: 1,
+ *         storageSize: 8192,
+ *         lastExtentSize: 8192,
+ *         paddingFactor: 1,
+ *         paddingFactorNote: 'paddingFactor is unused and unmaintained in 3.0. It remains hard coded to 1.0 for compatibility only.',
+ *         userFlags: 1,
+ *         capped: false,
+ *         nindexes: 2,
+ *         indexDetails: {},
+ *         totalIndexSize: 16352,
+ *         indexSizes: { _id_: 8176, loc_2d: 8176 },
+ *         ok: 1 },
+ *        ...
+ *        ]
+ *
+ * @apiUse UserNotAuthorized
+ */
+router.get('/stats',  auth.hasRole('admin'),controller.allStats);
 
 /**
-  * @api {put} /api/countries Update a country in database
-  * @apiName UpdateCountry
-  * @apiGroup Countries
-  *
-  * @apiUse ApiParams
-  *
-  * @apiSuccess {Object} Object response
-  *
-  * @apiSuccessExample Success-Response:
-  *   HTTP/1.1 201 Created:
-  *      {
-  *         "ok":1,
-  *         "nModified":0,
-  *         "upserted":[
-  *           {
-  *             "index":0,
-  *             "_id":"34347838758deb5"
-  *           }
-  *         ]
-  *     }
-  *
-  */
-router.get('/hostInfos',  controller.hostInfos);
-router.get('/status',  controller.serverStatus);
+ * @api {get} /api/back/db/hostInfos Request host informations
+ * @apiVersion 1.0.0
+ * @apiName GetHostInfos
+ * @apiGroup MongoDB
+ *
+ * @apiSuccess {Object} mongoDB host informations
+ *
+ * @apiSuccessExample Success-Response:
+ *   HTTP/1.1 200 OK
+ *    {"system":
+ *       {"currentTime":"2015-12-17T00:14:26.193Z",
+ *        "hostname":"MBP-0x42.local",
+ *        "cpuAddrSize":64,
+ *        "memSizeMB":8192,
+ *        "numCores":4,
+ *        "cpuArch":"x86_64\u0000",
+ *        "numaEnabled":false},
+ *       "os":{
+ *          "type":"Darwin",
+ *          "name":"Mac OS X",
+ *          "version":"15.2.0\u0000"},
+ *        ok":1}
+ *
+ * @apiUse UserNotAuthorized
+ */
+router.get('/hostInfos', auth.hasRole('admin'), controller.hostInfos);
+
+/**
+ * @api {get} /api/back/db/status Request host status
+ * @apiVersion 1.0.0
+ * @apiName GetHostStatus
+ * @apiGroup MongoDB
+ *
+ * @apiSuccess {Object} mongoDB host status
+ *
+ * @apiSuccessExample Success-Response:
+ *   HTTP/1.1 200 OK
+ *    {"host":"MBP-0x42.local",
+ *      "version":"3.0.7",
+ *      "process":"mongod",
+ *      "pid":5486,
+ *      "uptime":29963,
+ *      "uptimeMillis":29963101,
+ *      "uptimeEstimate":15411,
+ *      "localTime":"2015-12-17T00:17:09.164Z",
+ *      "asserts":{
+ *        "regular":0,
+ *        "warning":0,
+ *        "msg":0,
+ *        "user":4,
+ *        "rollovers":0},
+ *      ...}
+ *
+ * @apiUse UserNotAuthorized
+ */
+router.get('/status', auth.hasRole('admin'), controller.serverStatus);
 
 
 module.exports = router;
