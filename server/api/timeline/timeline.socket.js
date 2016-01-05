8d17e96a0fb2f5ee68024400edfa436661fea444
breizhers <yoann.diquelou@gmail.com>
2016-01-05 16:00:42 +0100

    Remove logging statements

--- server/api/timeline/timeline.socket.js
+++ server/api/timeline/timeline.socket.js
@@ -8,16 +8,12 @@ var Timeline = require('./timeline.model');
 
 exports.register = function(socket) {
   Timeline.schema.post('save', function (doc) {
-    console.log('SAVE');
     onSave(socket, doc);
   });
   Timeline.schema.post('remove', function (doc) {
-    console.log('REMOVE');
     onRemove(socket, doc);
   });
   Timeline.schema.post('findOneAndUpdate', function(doc){
-    console.log('UPDATE');
-    console.log(doc);
     onSave(socket, doc);
   });
 };
