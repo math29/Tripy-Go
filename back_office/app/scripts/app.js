bb5e15de9315faf9b96189c25cd7d6e5468c88d9
breizhers <yoann.diquelou@gmail.com>
2016-01-09 14:48:13 +0100

    Update back office for production

--- back_office/app/scripts/app.js
+++ back_office/app/scripts/app.js
@@ -144,8 +144,7 @@ angular
           'scripts/directives/timeline/timeline.js',
           'scripts/directives/dashboard/stats/stats.js',
           'scripts/factories/socket.service.js',
-          'bower_components/angular-socket-io/socket.js',
-          'bower_components/angular-socket-io/socket.min.js',
+          'bower_components/socket.io-client/socket.io.js',
           'bower_components/lodash/lodash.js'
            ]
         });
