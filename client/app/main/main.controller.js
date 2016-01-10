d7b301dc5a94455276455c5300800bb263446378
breizhers <yoann.diquelou@gmail.com>
2016-01-10 20:16:36 +0100

    Amélioration, du script de compilation

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -14,9 +14,10 @@ angular.module('wtcApp')
             });
         };
 
-        $scope.goAdmin = function(){
-          $window.location = '/back';
-        };
+
+    $scope.goAdmin = function(){
+              $window.location = '/back';
+            };
 
         for (var i=0; i<4; i++) {
             $scope.addSlide();
--- compile.sh
+++ compile.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 bower install
+bower update
 cd back_office && bower install
+bower update
 cd ..
 npm install
+npm update
 grunt build
