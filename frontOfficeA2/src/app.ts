828a117470897a46c69aa48a9e66432e93d5b6fd
math29 <maaath29@gmail.com>
2016-02-21 15:37:19 +0100

    Add bower + assets

--- frontOfficeA2/src/app.ts
+++ frontOfficeA2/src/app.ts
@@ -1,5 +1,13 @@
 import {bootstrap} from 'angular2/platform/browser';
+import {provide} from 'angular2/core';
+import {
+	ROUTER_PROVIDERS,
+	ROUTER_PRIMARY_COMPONENT
+} from 'angular2/router';
 import {FrontOfficeA2App} from './app/front-office-a2';
 
 
-bootstrap(FrontOfficeA2App);
+bootstrap(FrontOfficeA2App, [
+	ROUTER_PROVIDERS,
+	provide(ROUTER_PRIMARY_COMPONENT, {useValue: FrontOfficeA2App})
+]);
--- frontOfficeA2/src/app/front-office-a2.ts
+++ frontOfficeA2/src/app/front-office-a2.ts
@@ -1,16 +1,26 @@
 import {Component} from 'angular2/core';
+import {
+	RouteConfig,
+	RouteParams,
+	Route,
+	ROUTER_DIRECTIVES,
+	Router
+} from 'angular2/router';
 
 
 @Component({
   selector: 'front-office-a2-app',
   providers: [],
   templateUrl: 'app/front-office-a2.html',
-  directives: [],
+  directives: [ROUTER_DIRECTIVES],
   pipes: []
 })
 export class FrontOfficeA2App {
   defaultMeaning: number = 42;
-  
+
+  constructor(public router: Router) {
+  }
+
   meaningOfLife(meaning) {
     return `The meaning of life is ${meaning || this.defaultMeaning}`;
   }
