e4c1ddae306dff1a6afa028024d6c973d9ea7d9e
breizhers <yoann.diquelou@gmail.com>
2016-03-07 18:06:34 +0100

    clean code

--- server/api/language/language.socket.js
+++ server/api/language/language.socket.js
@@ -10,18 +10,13 @@ exports.register = function(socket) {
   Language.schema.post('save', function (doc) {
     onSave(socket, doc);
   });
-  Language.schema.post('update', function(){
-    //onSave(socket, doc);
 
-    console.log('post update');
-  });
   Language.schema.post('remove', function (doc) {
     onRemove(socket, doc);
   });
 };
 
 function onSave(socket, doc) {
-  console.log("save language");
   socket.emit('language:save', doc);
 }
 
