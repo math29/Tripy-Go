9c353cf1500cd4fc1559a82c5c427c07c67a53c1
breizhers <yoann.diquelou@gmail.com>
2016-03-17 14:28:17 +0100

    update back_office services

---
+++ back_office/app/scripts/classes/transport.ts
@@ -0,0 +1,22 @@
+import {Language} from './language';
+/**
+ * Classe définissant un language parlé ou écrit
+ **/
+export class Transport{
+
+  _id:string;
+  transportType: any;
+  distance: number;
+  classe: string;
+  date: any;
+  note: string;
+  barcode: string;
+  company: string;
+  departure: string;
+  arrival: string;
+
+  constructor(name:string, img:string){
+
+  }
+
+}
---
+++ back_office/app/scripts/classes/transportType.ts
@@ -0,0 +1,15 @@
+import {Language} from './language';
+/**
+ * Classe définissant un language parlé ou écrit
+ **/
+export class TransportType{
+
+  _id:string;
+  name: string;
+  img: string;
+
+  constructor(name:string, img:string){
+
+  }
+
+}
