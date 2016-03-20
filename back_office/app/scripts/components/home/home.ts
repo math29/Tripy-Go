dc1115dbab568c37530317f176eb18679f1dce20
breizhers <yoann.diquelou@gmail.com>
2016-03-20 16:51:13 +0100

    Update stars rate component

--- back_office/app/scripts/components/home/home.ts
+++ back_office/app/scripts/components/home/home.ts
@@ -2,12 +2,13 @@ import {Component} from 'angular2/core';
 import {Cookie} from 'ng2-cookies/ng2-cookies';
 import {Location, RouteConfig, RouterLink, Router, ROUTER_DIRECTIVES} from 'angular2/router';
 import {AuthService} from '../../tripy-lib/services/auth.service';
+import {StarsRateCmp} from '../../tripy-lib/components/rate/starsRate';
 
 @Component({
   selector: 'home',
   templateUrl: 'views/dashboard/home.html',
   providers: [],
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, StarsRateCmp],
   pipes: []
 })
 export class HomeCmp{
--- tripyGo_Libs/components/rate/starsRate.ts
+++ tripyGo_Libs/components/rate/starsRate.ts
@@ -5,93 +5,42 @@ import {Location, RouteConfig, RouterLink, Router, ROUTER_DIRECTIVES} from 'angu
 @Component({
   selector: 'stars-rate',
   template: `
-  <div class="container">
-    <div class="row">
-        <div class="col-xs-12 col-md-6">
+
             <div class="well well-sm">
                 <div class="row">
                     <div class="col-xs-12 col-md-6 text-center">
-                        <h1 class="rating-num">
-                            4.0</h1>
+                        <h1 class="rating-num">{{globalRate | number:'1.1-2'}}</h1>
                         <div class="rating">
-                            <span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star">
-                            </span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star">
-                            </span><span class="glyphicon glyphicon-star-empty"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star-empty"></span>
                         </div>
                         <div>
-                            <span class="glyphicon glyphicon-user"></span>1,050,008 total
+                            <span class="glyphicon glyphicon-user"></span>{{userNumber}} votants
                         </div>
                     </div>
-                    <div class="col-xs-12 col-md-6">
-                        <div class="row rating-desc">
+                    <div class="col-xs-12 col-md-6" >
+                        <div class="row rating-desc" *ngFor="#star of starsRange">
                             <div class="col-xs-3 col-md-3 text-right">
-                                <span class="glyphicon glyphicon-star"></span>5
+                                <span class="glyphicon glyphicon-star"></span>{{star.star}}
                             </div>
                             <div class="col-xs-8 col-md-9">
-                                <div class="progress progress-striped">
-                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20"
-                                        aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-                                        <span class="sr-only">80%</span>
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-{{star.class}}" role="progressbar" aria-valuenow="20"
+                                        aria-valuemin="0" aria-valuemax="100" style="width: {{star.value}}%">
+                                        <span class="sr-only">{{star.value}}%</span>
                                     </div>
                                 </div>
                             </div>
                             <!-- end 5 -->
-                            <div class="col-xs-3 col-md-3 text-right">
-                                <span class="glyphicon glyphicon-star"></span>4
-                            </div>
-                            <div class="col-xs-8 col-md-9">
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20"
-                                        aria-valuemin="0" aria-valuemax="100" style="width: 60%">
-                                        <span class="sr-only">60%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- end 4 -->
-                            <div class="col-xs-3 col-md-3 text-right">
-                                <span class="glyphicon glyphicon-star"></span>3
-                            </div>
-                            <div class="col-xs-8 col-md-9">
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20"
-                                        aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-                                        <span class="sr-only">40%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- end 3 -->
-                            <div class="col-xs-3 col-md-3 text-right">
-                                <span class="glyphicon glyphicon-star"></span>2
-                            </div>
-                            <div class="col-xs-8 col-md-9">
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="20"
-                                        aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-                                        <span class="sr-only">20%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- end 2 -->
-                            <div class="col-xs-3 col-md-3 text-right">
-                                <span class="glyphicon glyphicon-star"></span>1
-                            </div>
-                            <div class="col-xs-8 col-md-9">
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80"
-                                        aria-valuemin="0" aria-valuemax="100" style="width: 15%">
-                                        <span class="sr-only">15%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- end 1 -->
                         </div>
                         <!-- end row -->
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
+
   `,
   styles:['body{ margin-top:20px;}',
 '.glyphicon { margin-right:5px;}',
@@ -107,14 +56,41 @@ import {Location, RouteConfig, RouterLink, Router, ROUTER_DIRECTIVES} from 'angu
 export class StarsRateCmp{
   private idUp:string;
   private idDown:string;
-  
+  private globalRate: number = 4.0;
+  private userNumber: number = 10;
+  private starsRange: any[] = [{
+    star: 4,
+    value: 80,
+    class: 'success'
+  },
+  {
+    star: 4,
+    value: 60,
+    class: 'success'
+  },
+  {
+    star: 3,
+    value: 40,
+    class:'info'
+  },
+  {
+    star: 2,
+    value: 20,
+    class:'warning'
+  },
+  {
+    star: 1,
+    value: 10,
+    class: 'danger'
+  }
+];
 
-    constructor(){
-    }
+constructor(){
+}
 
-    ngOnInit(){
-    }
+ngOnInit(){
+}
 
-    ngOnDestroy(){
-    }
+ngOnDestroy(){
+}
 }
