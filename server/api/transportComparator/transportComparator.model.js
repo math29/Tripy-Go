cbee55f12821b26575b57f0bb9efd74a610ed12a
breizhers <yoann.diquelou@gmail.com>
2016-03-30 11:10:58 +0200

    create transport comparator model

---
+++ server/api/transportComparator/transportComparator.model.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+var TransportComparatorSchema = new Schema({
+  type: [{
+    type: Schema.Types.ObjectId,
+    ref: 'TransportType',
+    required: true
+  }],
+  company:{
+    type: Schema.Types.ObjectId,
+    ref: 'Company',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('TransportComparator', TransportSchema);
