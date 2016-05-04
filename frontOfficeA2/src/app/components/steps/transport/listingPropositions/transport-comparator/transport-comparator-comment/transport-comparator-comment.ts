88cc789445953ae49b67f8e6d44f5da065407992
math29 <maaath29@gmail.com>
2016-05-04 17:29:30 +0200

    Add initiale comment comparator design

--- frontOfficeA2/src/app/components/steps/transport/listingPropositions/transport-comparator/transport-comparator-comment/transport-comparator-comment.ts
+++ frontOfficeA2/src/app/components/steps/transport/listingPropositions/transport-comparator/transport-comparator-comment/transport-comparator-comment.ts
@@ -13,7 +13,7 @@ import { AuthService } from '../../../../../../tripy_go_lib/services/auth.servic
 })
 export class TransportComparatorComment {
 	private comment: any;
-	private vote_side: String;
+	private rate_side: String;
 	private options_post: RequestOptions;
   
 	constructor(private _auth: AuthService, private _http: Http) {
@@ -21,12 +21,25 @@ export class TransportComparatorComment {
 	}
 
 	ngOnInit(){
-		console.log(this.comment);
+		// Initialize aspect
+		let rate = this.comment.rate;
+		for (var i = 0; i < rate.raters.length; i++){
+			if(rate.raters[i].user == this._auth.getMe()._id){
+				switch (rate.raters[i].action){
+					case 1:
+						this.rate_side = "up";
+						break;
+					case -1:
+						this.rate_side = "down";
+						break;
+				}
+			}
+		}
 	}
 
 	// Rate Votes
 	vote(side) {
-		this.vote_side = side;
+		this.rate_side = side;
 
 		// Post Vote
 		this._http.post(`/api/rate/vote/${side}/${this.comment.rate._id}`, null, this.options_post)
