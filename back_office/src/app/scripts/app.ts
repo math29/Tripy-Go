d63da2c90510bb43fd47fd32cd1215e3a6a59059
breizhers <yoann.diquelou@gmail.com>
2016-05-19 13:04:25 +0200

    Update back office config

--- back_office/src/app/scripts/app.ts
+++ back_office/src/app/scripts/app.ts
@@ -1,10 +1,8 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
-import { provide } from 'angular2/core';
 import { FORM_PROVIDERS } from 'angular2/common';
 
 import {ROUTER_PROVIDERS, ROUTER_PRIMARY_COMPONENT} from 'angular2/router';
-import { AuthConfig, AuthHttp } from 'angular2-jwt/angular2-jwt';
 
 import {Tripy_Back} from './back';
 import {AuthService} from './tripy-lib/services/auth.service';
@@ -16,14 +14,5 @@ bootstrap(Tripy_Back, [
   HTTP_PROVIDERS,
   ROUTER_PROVIDERS,
   AuthService,
-  SocketService,
-  provide(AuthHttp,{
-    useFactory:(http) => {
-      return new AuthHttp(new AuthConfig({
-        tokenName: 'jwt'
-      }), http);
-    },
-
-    deps: [Http]
-  })
+  SocketService
 ]);
