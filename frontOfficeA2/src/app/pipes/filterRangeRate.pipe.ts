e77bca32d26fc50ffb2954cb1e990dd09e5529df
breizhers <yoann.diquelou@gmail.com>
2016-05-20 23:38:27 +0200

    Revert "Update front for working, nearly OK"
    This reverts commit 9c549b54644ceb12426384f1b9d53885a8e563eb.

--- frontOfficeA2/src/app/pipes/filterRangeRate.pipe.ts
+++ frontOfficeA2/src/app/pipes/filterRangeRate.pipe.ts
@@ -7,8 +7,8 @@ export class filterRangeRate implements PipeTransform {
 	transform(allComparators: any, args: string[]) {
 		if (allComparators) {
 			return allComparators.filter(comparator => {
-				if (comparator.transport.ergo_rate.score >= args[0][0] && comparator.transport.ergo_rate.score <= args[0][1]
-					&& comparator.transport.ergo_rate.score >= args[1][0] && comparator.transport.ergo_rate.score <= args[1][1]) {
+				if (comparator.ergo_rate.score >= args[0][0] && comparator.ergo_rate.score <= args[0][1]
+					&& comparator.ergo_rate.score >= args[1][0] && comparator.ergo_rate.score <= args[1][1]) {
 					return true;
 				}
 			});
