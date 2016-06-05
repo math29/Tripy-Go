5de3929cdb110f12b24016d00cb56fc369baa3bf
Yoann Diquélou <breizhers@users.noreply.github.com>
2016-06-05 17:06:21 +0200

    Delete proxy.sh
    Criticals informations

--- proxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-export http_proxy=http://y1diquel:FNz*5046tf?@proxy.enib.fr:3128
-export https_proxy=http://y1diquel:FNz*5046tf?@proxy.enib.fr:3128
-npm config set proxy http://y1diquel:FNz*5046tf?@proxy.enib.fr:3128
-npm config set https-proxy http://y1diquel:FNz*5046tf?@proxy.enib.fr:3128
-
